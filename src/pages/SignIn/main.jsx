e99464106233c496a1e9eecd5289b2bf73f7a139
ana-carolsoares <97407994+ana-carolsoares@users.noreply.github.com>
2023-06-28 22:50:54 -0300

    página login

--- src/pages/SignIn/main.jsx
+++ src/pages/SignIn/main.jsx
@@ -1,8 +1,41 @@
+import './signin.css'
+import logo from '../../assets/logo.png';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function SignIn(){
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
     return(
-        <div>
-            <h1>Página Login</h1>
+        <div className='contaiber-centre'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt="logo do sistema de chamados" />
+                </div>
+
+                <form>
+                    <h1>Entrar</h1>
+                    <input 
+                    type="text" 
+                    placeholder='email@email.com'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <input 
+                    type="password" 
+                    placeholder='****'
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                    />
+
+                    <button type='submit'>Acessar</button>
+                </form>
+
+                <Link to="/registro">Criar uma conta</Link>
+            </div>
         </div>
     )
 }
--- src/pages/SignUp/main.jsx
+++ src/pages/SignUp/main.jsx
@@ -1,4 +1,6 @@
 
+
+
 export default function SignUp(){
     return(
         <div>
