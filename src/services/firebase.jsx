bd6746a314476e853085db315596bc1fe3c130b0
ana-carolsoares <97407994+ana-carolsoares@users.noreply.github.com>
2023-07-04 21:27:06 -0300

    firebase

---
+++ src/services/firebase.jsx
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth} from 'firebase/auth'
+import { getFirebaseStore, getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDr88rCRHbocyiiDBpivMwYXL8i19lYtrs",
+    authDomain: "tickets-7832f.firebaseapp.com",
+    projectId: "tickets-7832f",
+    storageBucket: "tickets-7832f.appspot.com",
+    messagingSenderId: "696711075825",
+    appId: "1:696711075825:web:ddc7f12c124c75f473d650",
+    measurementId: "G-R4K0Q0Y2C7"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const suth = getAuth(firebaseApp);
+
+const db = getFirestore(firebaseApp);
+
+const storage = getStorage(firebaseApp); 
+
+export { auth, db, storage}
+
